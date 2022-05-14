b5bdc41e988c5a8e5a05e62fa0b8b28c3bd0b9d2
bosd <c5e2fd43-d292-4c90-9d1f-74ff3436329a@anonaddy.me>
2022-05-14 14:34:05 +0200

    add l10n_nl_business_document_import

---
+++ l10n_nl_business_document_import/tests/test_business_document_import.py
@@ -0,0 +1,35 @@
+# Copyright 2022 bosd
+# @author: bosd <c5e2fd43-d292-4c90-9d1f-74ff3436329a@anonaddy.me>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.tests.common import TransactionCase
+
+
+class Testl10nNLBusinessDocumentImport(TransactionCase):
+    def test_match_partner_coc_oin(self):
+        partner1 = self.env["res.partner"].create(
+            {
+                "name": "Onestein",
+                "supplier_rank": 1,
+                "is_company": True,
+                "coc_registration_number": "56048785",
+            }
+        )
+        partner2 = self.env["res.partner"].create(
+            {
+                "name": "Partner with OIN",
+                "supplier_rank": 1,
+                "is_company": True,
+                "nl_oin": "12345678901234567890",
+            }
+        )
+        bdio = self.env["business.document.import"]
+        partner_dict = {"coc_registration_number": "56048785"}
+        res = bdio._match_partner(partner_dict, [])
+        self.assertIn(res, [partner1, partner2])
+        partner_dict = {"coc_registration_number": "56048785"}
+        res = bdio._match_partner(partner_dict, [])
+        self.assertEqual(res, partner1)
+        partner_dict = {"nl_oin": "12345678901234567890"}
+        res = bdio._match_partner(partner_dict, [])
+        self.assertIn(res, [partner1, partner2])
